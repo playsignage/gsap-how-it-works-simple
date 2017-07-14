04613c6a3a39cd712f423f5bb18d098ebb51caa0
Dan Mindru <mindrudan@gmail.com>
2017-07-14 17:48:17 +0200

    Make pipe and vertical pipe fade; accept animation start delay

--- svg/Pipe.js
+++ svg/Pipe.js
@@ -1,23 +1,36 @@
+import { Component } from 'preact'
+import uuid from 'uuid'
 import { colors } from '../config'
 
-const Pipe = ({ customWidth = 100, customHeight = 2, ...props }) => {
-  const movingPipeLength = 10
-  const ContainingRect = (props) => (<rect {...props} fill={colors.lightSkyBlue} x="0" y="0" width={customWidth} height={customHeight} />)
+export default class Pipe extends Component {
+  constructor () {
+    super()
 
-  return (
-    <g {...props}>
-      <ContainingRect />
+    this.state = {
+      internalId: uuid().replace(/-/g,'')
+    }
+  }
 
-      <g>
-        <rect fill={colors.denim} x="0" y="0" width={movingPipeLength} height={customHeight} />
-        <animateMotion dur="2s" repeatCount="indefinite">
-          <mpath xlinkHref="#pipeMotionPath" />
-        </animateMotion>
+  render ({ customWidth = 100, customHeight = 2, customDuration = 2, customDelay = 1, ...props }, { internalId }) {
+    const movingPipeLength = 10
+    const ContainingRect = (props) => (<rect {...props} fill={colors.lightSkyBlue} x="0" y="0" width={customWidth} height={customHeight} />)
+
+    return (
+      <g {...props}>
+        <ContainingRect />
+
+        <g>
+          <rect fill={colors.denim} x="0" y="0" width={movingPipeLength} height={customHeight}>
+            <animate attributeType="XML" attributeName="opacity" values="0.5;1;0" dur={`${customDuration}s`} repeatCount="indefinite" />
+          </rect>
+
+          <animateMotion dur={`${customDuration}s`} repeatCount="indefinite">
+            <mpath xlinkHref={`#pipeMotionPath${internalId}`} />
+          </animateMotion>
+        </g>
+
+        <path d={`M0,0 ${customWidth - movingPipeLength},0`} id={`pipeMotionPath${internalId}`} />
       </g>
-
-      <path d={`M0,0 ${customWidth - movingPipeLength},0`} id="pipeMotionPath" />
-    </g>
-  )
+    )
+  }
 }
-
-export default Pipe
--- svg/VerticalPipe.js
+++ svg/VerticalPipe.js
@@ -1,23 +1,36 @@
+import { Component } from 'preact'
+import uuid from 'uuid'
 import { colors } from '../config'
 
-const VerticalPipe = ({ customWidth = 2, customHeight = 100, ...props }) => {
-  const movingVerticalPipeLength = 10
-  const ContainingRect = (props) => (<rect {...props} fill={colors.lightSkyBlue} x="0" y="0" width={customWidth} height={customHeight} />)
+export default class VerticalPipe extends Component {
+  constructor () {
+    super()
 
-  return (
-    <g {...props}>
-      <ContainingRect />
+    this.state = {
+      internalId: uuid().replace(/-/g, '')
+    }
+  }
 
-      <g>
-        <rect fill={colors.denim} x="0" y="0" height={movingVerticalPipeLength} width={customWidth} />
-        <animateMotion dur="1s" repeatCount="indefinite">
-          <mpath xlinkHref="#verticalpipeMotionPath" />
-        </animateMotion>
+  render({ customWidth = 2, customHeight = 100, customDuration = 2, customDelay = 0, ...props }, { internalId }) {
+    const movingVerticalPipeLength = 10
+    const ContainingRect = (props) => (<rect {...props} fill={colors.lightSkyBlue} x="0" y="0" width={customWidth} height={customHeight} />)
+
+    return (
+      <g {...props}>
+        <ContainingRect />
+
+        <g>
+          <rect fill={colors.denim} x="0" y="0" height={movingVerticalPipeLength} width={customWidth}>
+            <animate attributeType="XML" attributeName="opacity" values="0;1;0.5" dur={`${customDuration}s`} repeatCount="indefinite" />
+          </rect>
+
+          <animateMotion begin={customDelay} dur={`${customDuration}s`} repeatCount="indefinite">
+            <mpath xlinkHref={`#verticalPipeMotionPath${internalId}`} />
+          </animateMotion>
+        </g>
+
+        <path d={`M0,0 0,${customHeight - movingVerticalPipeLength}`} id={`verticalPipeMotionPath${internalId}`} />
       </g>
-
-      <path d={`M0,0 0,${customHeight - movingVerticalPipeLength}`} id="verticalpipeMotionPath" />
-    </g>
-  )
+    )
+  }
 }
-
-export default VerticalPipe
