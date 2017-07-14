dbd48885275c9e932464b17fcaa6070dc2dcda07
Dan Mindru <mindrudan@gmail.com>
2017-07-14 17:47:52 +0200

    Finish up simple animation

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 import { Component } from 'preact'
 import {
   TimelineLite,
-  // SlowMo,
+  SlowMo,
   Power2
 } from 'gsap'
 import {
-  FontIcon,
-  ImageIcon,
-  TextIcon,
-  VideoIcon,
   Laptop,
-  Video,
   Pipe,
   VerticalPipe,
   Television,
@@ -24,6 +19,8 @@ import {
 
 import { qs } from './helpers'
 
+const timeline = new TimelineLite()
+
 export default class App extends Component {
   constructor () {
     super();
@@ -36,11 +33,18 @@ export default class App extends Component {
 
   componentDidMount () {
     const laptopDOM = qs('#laptop')
-
-    const timeline = new TimelineLite()
+    const laptopAndContentDOM = qs('#laptopAndContent')
+    const tvRightAndContentDOM = qs('#tvRightAndContent')
+    const tvLeftAndContentDOM = qs('#tvLeftAndContent')
+    const pipesDOM = qs('#pipes')
 
     timeline
       .to(laptopDOM, 0.5, { opacity: 1, ease: Power2.easeIn })
+      .to(laptopAndContentDOM, 0.5, { scale: 0.6, transformOrigin: 'center center', delay: 3, ease: SlowMo.easeOut })
+      .to(tvRightAndContentDOM, 0.5, { x: 550, ease: SlowMo.easeOut }, 'tvs')
+      .to(tvLeftAndContentDOM, 0.5, { x: 10, ease: SlowMo.easeOut }, 'tvs')
+      .to(tvLeftAndContentDOM, 0.5, { x: 10, ease: SlowMo.easeOut }, 'tvs')
+      .to(pipesDOM, 0.5, { opacity: 1 }, 'tvs+0.3')
 
     timeline.timeScale(this.state.timeScale)
   }
@@ -49,23 +53,26 @@ export default class App extends Component {
     return (
       <main>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 800 340">
-          <Laptop id="laptop" opacity="0" transform="translate(262.5, 80)" mainColor={mainColor} />
-          <Content id="laptopContent" transform="translate(262, 79.5)" mainColor={mainColor} timeScale={timeScale} delay="1" />
+          <g id="laptopAndContent">
+            <Laptop id="laptop" opacity="0" transform="translate(268, 80)" mainColor={mainColor} />
+            <Content id="laptopContent" transform="translate(267, 79.5)" mainColor={mainColor} timeScale={timeScale} delay="1" />
+          </g>
 
-          {/*
-          <FontIcon mainColor={mainColor} />
-          <ImageIcon mainColor={mainColor} />
-          <TextIcon mainColor={mainColor} />
-          <VideoIcon mainColor={mainColor} />
-          <Text mainColor={mainColor} />
-          <Headline />
-          <Image mainColor={mainColor} />
+          <g id="tvRightAndContent" transform="translate(800, 10)">
+            <Television id="tvRight" />
+            <Content id="tvRightContent" transform="translate(-25, -3), scale(1.18, 1.075)" mainColor={mainColor} timeScale={timeScale} delay="5.5" />
+          </g>
 
-          <Video mainColor={mainColor} />
-          <Pipe />
-          <VerticalPipe />
-          <Television />
-          */}
+          <g id="tvLeftAndContent" transform="translate(-240, 10)">
+            <Television id="tvLeft" />
+            <Content id="tvLeftContent" transform="translate(-25, -3), scale(1.18, 1.075)" mainColor={mainColor} timeScale={timeScale} delay="5.5" />
+          </g>
+
+          <g id="pipes" opacity="0">
+            <Pipe transform="translate(397.5, 76), rotate(180)" customWidth="137.5" customDelay="2" />
+            <Pipe transform="translate(397.5, 74)" customWidth="137.5" customDelay="2" />
+            <VerticalPipe transform="translate(398, 105), rotate(180)" customHeight="30" />
+          </g>
         </svg>
       </main>
     )
